d271e1f9e1c80af50013e41656cd6dda2e6a936e
MAIN\mxo7550 <mxo7550@rit.edu>
2019-02-13 10:54:56 -0500

    Started Hole, changed line color

---
+++ Line Drive/Assets/Scripts/Hole.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+    GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.Find("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name != "Ball") return;
+        Debug.Log("Level Complete!");
+    }
+}
