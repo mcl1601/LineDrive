5ccb97c6b0c04e29781323f5cf1b068322968901
Matt Lienhard <mcl1601>
2019-02-14 12:17:35 -0500

    Added in some art assets

---
+++ Line Drive/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    public GameObject ballPrefab;
+    public GameObject teeBoxInst;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
