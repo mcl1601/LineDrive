2aa902d230bb52de228e834d55a0ba3dd7278d33
MAIN\mxo7550 <mxo7550@rit.edu>
2019-02-15 10:52:14 -0500

    Added toggle to change between line and speed boosts. Still a little buggy.

--- Line Drive/Assets/Scripts/DrawLine.cs
+++ Line Drive/Assets/Scripts/DrawLine.cs
@@ -12,88 +12,94 @@ public class DrawLine : MonoBehaviour {
     bool wasDrawing = false;
     GameObject lineRef = null;
     Vector3 lastPoint = Vector3.zero;
+    string toggle = "";
 
     List<GameObject> lines = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
-		
-	}
+         
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        toggle = GameObject.Find("SceneManager").GetComponent<ToolToggle>().toggle;
         // quick reference to the current line we are drawing
-        LineRenderer l = null;
-        if (lineRef != null)
-            l = lineRef.GetComponent<LineRenderer>();
-
-        Vector3 mousPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f));
-        // while holding down the mouse or finger
-        if (Input.GetMouseButton(0))
+        if (toggle == "Line")
         {
-            // get the point
-
-            // if this is the first frame of drawing
-            if(!drawing)
-            {
-                // make a new line and save the reference
-                lineRef = Instantiate(linePre, Vector3.zero, Quaternion.identity);
-                drawing = true;
+            LineRenderer l = null;
+            if (lineRef != null)
                 l = lineRef.GetComponent<LineRenderer>();
-                // set the frst point of the line and add it to the list
-                l.SetPosition(0, mousPos);
-                lastPoint = mousPos;
-                lines.Add(lineRef);
-                return;
-            }
 
-            wasDrawing = true;
-
-            // not first frame of drawing
-            // have we moved enough to add another segment?
-            if ((lastPoint - mousPos).magnitude < minDrawDistance) return;
-            
-            // add a new point at the current location
-            l.positionCount++;
-            l.SetPosition(l.positionCount - 1, mousPos);
-            lastPoint = mousPos;
-
-        }
-        // first frame after not drawing anymore
-        else if(wasDrawing)
-        {
-            drawing = false;
-            wasDrawing = false;
-            if(l.positionCount == 1)
+            Vector3 mousPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f));
+            // while holding down the mouse or finger
+            if (Input.GetMouseButton(0))
             {
+                // get the point
+
+                // if this is the first frame of drawing
+                if (!drawing)
+                {
+                    // make a new line and save the reference
+                    lineRef = Instantiate(linePre, Vector3.zero, Quaternion.identity);
+                    drawing = true;
+                    l = lineRef.GetComponent<LineRenderer>();
+                    // set the frst point of the line and add it to the list
+                    l.SetPosition(0, mousPos);
+                    lastPoint = mousPos;
+                    lines.Add(lineRef);
+                    return;
+                }
+
+                wasDrawing = true;
+
+                // not first frame of drawing
+                // have we moved enough to add another segment?
+                if ((lastPoint - mousPos).magnitude < minDrawDistance) return;
+
+                // add a new point at the current location
                 l.positionCount++;
-                l.SetPosition(1, mousPos + new Vector3(0.1f, 0f, 0f));
+                l.SetPosition(l.positionCount - 1, mousPos);
+                lastPoint = mousPos;
+
             }
-            // set up the edge collider using the line points
-            EdgeCollider2D e = lineRef.GetComponent<EdgeCollider2D>();
-            Vector2[] points = new Vector2[l.positionCount];
-            for(int i = 0; i < l.positionCount; i++)
+            // first frame after not drawing anymore
+            else if (wasDrawing)
             {
-                points[i] = new Vector2(l.GetPosition(i).x, l.GetPosition(i).y);
+                drawing = false;
+                wasDrawing = false;
+                if (l.positionCount == 1)
+                {
+                    l.positionCount++;
+                    l.SetPosition(1, mousPos + new Vector3(0.1f, 0f, 0f));
+                }
+                // set up the edge collider using the line points
+                EdgeCollider2D e = lineRef.GetComponent<EdgeCollider2D>();
+                Vector2[] points = new Vector2[l.positionCount];
+                for (int i = 0; i < l.positionCount; i++)
+                {
+                    points[i] = new Vector2(l.GetPosition(i).x, l.GetPosition(i).y);
+                }
+                e.points = points;
+
+                // add a rigidbody for gravity
+                if (lineHasPhysics)
+                    lineRef.AddComponent<Rigidbody2D>();
             }
-            e.points = points;
 
-            // add a rigidbody for gravity
-            if (lineHasPhysics)
-                lineRef.AddComponent<Rigidbody2D>();
-        }
+            // make the ball roll
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ball.AddComponent<Rigidbody2D>();
+                ball.GetComponent<Rigidbody2D>().mass = 0.2f;
+            }
 
-        // make the ball roll
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            ball.AddComponent<Rigidbody2D>();
-            ball.GetComponent<Rigidbody2D>().mass = 0.2f;
+            // delete the last made line
+            if (Input.GetKeyDown(KeyCode.Z) && lines.Count > 0)
+            {
+                Destroy(lines[lines.Count - 1]);
+                lines.RemoveAt(lines.Count - 1);
+            }
         }
-
-        // delete the last made line
-        if(Input.GetKeyDown(KeyCode.Z) && lines.Count > 0)
-        {
-            Destroy(lines[lines.Count - 1]);
-            lines.RemoveAt(lines.Count - 1);
-        }
+        
 	}
 }
---
+++ Line Drive/Assets/Scripts/PlaceBoost.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBoost : MonoBehaviour {
+    public GameObject speedBoost;
+    string toggle;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        toggle = this.gameObject.GetComponent<ToolToggle>().toggle;
+        if(Input.GetMouseButtonDown(0) && toggle == "Boost")
+        {
+            Debug.Log("Yeet");
+            Vector3 mousPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f));
+            Instantiate(speedBoost, mousPos, Quaternion.identity);
+        }
+    }
+   
+}
