d525a5476ad634dabe37831472349a3c5a360290
MAIN\mxo7550 <mxo7550@rit.edu>
2019-02-18 10:48:38 -0500

    Added a way to move through levels and keep track of max level.

--- Line Drive/Assets/Scripts/Hole.cs
+++ Line Drive/Assets/Scripts/Hole.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Hole : MonoBehaviour {
     GameObject ball;
-
+    int currentLevel;
+    int maxLevel = 1;
 	// Use this for initialization
 	void Start () {
         ball = GameObject.Find("Ball");
+        //If we're at the first hole, initialize the current hole at 1
+        if(SceneManager.GetActiveScene().name == "Hole1")
+        {
+            currentLevel = 1;
+        }
+        //If not, set the current level to what it's saved as in the playerPref
+        else
+        {
+            currentLevel = PlayerPrefs.GetInt("CurrentLevel");
+        }
+        //If the current level we're on exceeds the recorded max level, make this level the new max level
+        if(currentLevel > maxLevel)
+        {
+            maxLevel = currentLevel;
+            PlayerPrefs.SetInt("maxLevel", maxLevel);
+        }
 	}
 	
 	// Update is called once per frame
@@ -16,7 +34,17 @@ public class Hole : MonoBehaviour {
 	}
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Only check for ball collision
         if (collision.gameObject.name != "Ball") return;
-        Debug.Log("Level Complete!");
+        //Make it so we can't go past the max Level
+        if(currentLevel == maxLevel)
+        {
+            maxLevel++;
+            PlayerPrefs.SetInt("maxLevel", maxLevel);
+        }
+        //Increment the current level and use that to go to the next scene
+        currentLevel += 1;
+        PlayerPrefs.SetInt("CurrentLevel", currentLevel);
+        SceneManager.LoadScene("Hole" + currentLevel);
     }
 }
