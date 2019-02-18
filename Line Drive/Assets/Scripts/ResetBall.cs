22e780c4e2fa0b2cdbdc1be92bbcd13a6372fd06
James Troup <jlt9768@ad.rit.edu>
2019-02-18 10:31:04 -0500

    Added a ResetBall script to the ball prefab
    Also created Hole2

---
+++ Line Drive/Assets/Scripts/ResetBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBall : MonoBehaviour {
+    Vector3 startPos;
+    // Use this for initialization
+    void Start () {
+        startPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.y <= -6)
+        {
+            
+            this.transform.position = startPos;
+            Destroy(this.GetComponent<Rigidbody2D>());
+        }
+	}
+}
