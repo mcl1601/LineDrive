a0fb78e719b383bdd7f39404f0b6f435aa4bb7d0
Matt Lienhard <mcl1601>
2019-02-28 19:26:04 -0500

    Added animated score screen

--- Line Drive/Assets/Scripts/ScoreManager.cs
+++ Line Drive/Assets/Scripts/ScoreManager.cs
@@ -15,8 +15,11 @@ public class ScoreManager : MonoBehaviour {
     public int stars = 0;
     public GameObject starObj;
     int totalScore = 0;
+    
+    int totalBoost = 0;
 	void Start () {
         lineJuice = GameObject.Find("Main Camera").GetComponent<DrawLine>().lineJuice;
+        totalBoost = gameObject.GetComponent<PlaceBoost>().maxBoosts;
 	}
 	
 	// Update is called once per frame
@@ -33,7 +36,7 @@ public class ScoreManager : MonoBehaviour {
         else if (lineJuice >= star1val) stars = 1;
         else stars = 0;
         totalScore = lineJuice * 100;
-        GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = "Score: " + totalScore;
+        GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = "Score: 0";
         StartCoroutine(SpawnStars());
        
     }
@@ -50,5 +53,41 @@ public class ScoreManager : MonoBehaviour {
             stars--;
             yield return new WaitForSeconds(0.4f);
         }
+        StartCoroutine(ShowScoreText());
+    }
+
+    IEnumerator ShowScoreText()
+    {
+        TextMeshProUGUI s = GameObject.Find("ScorePerComponent").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI total = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+        s.text = "Line Meter Remaining = +" + (lineJuice * 100);
+        yield return new WaitForSeconds(0.5f);
+        float percent = 0f;
+        float timer = 0f;
+        float displayVal = 0;
+        while(percent < 1f)
+        {
+            percent = timer / 0.5f;
+            displayVal = Mathf.Floor(Mathf.Lerp(0f, lineJuice * 100f, percent));
+            total.text = "Total Score: " + displayVal;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        timer = 0f;
+        percent = 0f;
+        s.text = "Boosts Leftover ("+extraBoosts+" / "+totalBoost+") = +" + (extraBoosts * 500);
+        float initScore = displayVal;
+        displayVal = 0;
+        while (percent < 1f)
+        {
+            percent = timer / 0.5f;
+            displayVal = Mathf.Floor(Mathf.Lerp(initScore, (extraBoosts * 500) + initScore, percent));
+            total.text = "Total Score: " + displayVal;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        s.text = "";
     }
 }
