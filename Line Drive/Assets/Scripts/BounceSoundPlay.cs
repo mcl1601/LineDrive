cecd1382462e71af49d2cbc3fc5498ff4f29ffb4
MAIN\mxo7550 <mxo7550@rit.edu>
2019-03-06 10:46:58 -0500

    Implemented boost se, fixed Play On Awake for all scenes.

--- Line Drive/Assets/Scripts/BounceSoundPlay.cs
+++ Line Drive/Assets/Scripts/BounceSoundPlay.cs
@@ -14,7 +14,7 @@ public class BounceSoundPlay : MonoBehaviour {
 		
 	}
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name != "Ball") return;
         GetComponent<AudioSource>().Play();
